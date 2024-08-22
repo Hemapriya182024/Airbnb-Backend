9ddb0238cd557a902fb550d3820a435712cff16d
Hemapriya182024 <hemakarthikeyan2024@gmail.com>
2024-08-22 20:12:23 +0530

    auth-updated

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
-router.get('/profile', AuthMiddleware, AuthController.profile);
+router.get('/profile', AuthController.profile);
 router.post('/logout', AuthController.logout);
 
 module.exports = router;
